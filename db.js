0eff10be66fe0c3daea83db703d25e9564e94a36
julia0arias <julia.arias.navas@gmail.com>
2023-03-27 19:24:34 +0200

    Delete productos

--- db.js
+++ db.js
@@ -4,14 +4,16 @@ const fs = require('fs');
 const pool = new Pool(JSON.parse(fs.readFileSync(__dirname + '/public/db_credentials.json')));
 
 const db = {
+    //query
     query: async (str) => {
         pool.connect();
         return await pool.query(str);
     },
-    /*execute: async (str) => {
+    //modificar datos
+    execute: async (str) => {
         pool.connect()
         await pool.execute(str);
-    }*/
+    }
 };
 
 module.exports = db;
--- routes/productos.js
+++ routes/productos.js
@@ -7,10 +7,24 @@ router.get('/', async(req, res) => {
     res.json(result.rows);
 });
 
-router.get('/getByID', async(req, res) => {
-    console.log(data);
-    let result = await db.query('select * from products where id = ' + req.params.id);
+router.delete('/:id', async(req, res) => {
+
+    await db.execute('DELETE FROM order_details where product_id = ' + req.params.id);
+    let result = await db.execute('DELETE FROM products where product_id = ' + req.params.id);
     res.json(result.rows);
 });
+
+router.put('/', async (req, res) => {
+    try {
+      const productId = req.params.id;
+      const product = req.body; 
+      await db.execute('UPDATE products SET name = ?, price = ?, description = ? WHERE product_id = ?', [product.name, product.price, product.description, productId]);
+      res.status(200).json({ message: `Producto con ID ${productId} actualizado correctamente.` });
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({ message: `Error al actualizar el producto con ID ${productId}.` });
+    }
+  });
+  
 
 module.exports = router;
