512dbe38fff1b2749a26776be3a81d8cd6327434
xavierqo.1990 <silent90s@gmail.com>
2023-04-01 12:57:38 +0200

    added create and delete, working on edit

--- routes/categorias.js
+++ routes/categorias.js
@@ -2,15 +2,61 @@ const express = require('express');
 const router = express.Router();
 const db = require('../db');
 
-router.get('/', async(req, res) => {
-    const categories = await db.query('SELECT * FROM categories');
-    res.json(categories['rows']);
+router.post('/', async (req,res) => {
+    try {
+        const { name, desc } = req.body;
+        const lastRow = await db.query('SELECT c.category_id FROM categories c ORDER BY c.category_id DESC LIMIT 1');
+        const categoryId = (lastRow.rows[0]['category_id']+1);
+        await db.query(`INSERT INTO categories(category_id,category_name,description) VALUES(${categoryId},'${name}','${desc}')`);
+        res.status(200).json({'message':'categoria añadida correctamente'});
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({
+            'message': 'Error añadiendo categoría',
+            'exception':e
+        })
+    }
 });
 
-router.delete('/', async(req, res) => {
-    await db.query('DELETE FROM products p WHERE p.category_id = ?', [req.body.category_id]);
-    console.log(await db.query('DELETE FROM categories c WHERE c.category_id = ?', [req.body.category_id]))
-    res.json({'message':'categoria eliminada correctamente'});
+router.get('/:id', async(req, res) => {
+    try {
+        const categories = await db.query(`SELECT * FROM categories WHERE category_id = ${req.params.id}`);
+        res.status(200).json(categories['rows'][0]);
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({
+            'message': 'Error solicitando categoría',
+            'exception':e
+        })
+    }
+});
+
+router.get('/', async(req, res) => {
+    try {
+        const categories = await db.query('SELECT * FROM categories');
+        res.status(200).json(categories['rows']);
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({
+            'message': 'Error solicitando categorías',
+            'exception':e
+        })
+    }
+});
+
+router.delete('/:id', async(req, res) => {
+    try {
+        const categoryId = req.params.id;
+        await db.query(`UPDATE products SET category_id = null WHERE category_id = ${categoryId}`);
+        await db.query(`DELETE FROM categories WHERE category_id = ${categoryId}`);
+        res.status(200).json({'message':'categoria eliminada correctamente'});
+    } catch (e) {
+        console.error(e)
+        res.status(500).json({
+            'message': 'Error eliminando categoría',
+            'exception':e
+        })
+    }
 });
 
 module.exports = router;
