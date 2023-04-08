27e7abc8147966a7a93f8a555defa2aa25ad02c4
julia0arias <julia.arias.navas@gmail.com>
2023-04-08 14:47:28 +0200

    Fix db query with optArray

--- db.js
+++ db.js
@@ -4,13 +4,10 @@ const fs = require('fs');
 const pool = new Pool(JSON.parse(fs.readFileSync(__dirname + '/public/db_credentials.json')));
 
 const db = {
-    query: async (str) => {
+    //query
+    query: async (str, optArray) => {
         pool.connect();
-        return await pool.query(str);
-    },
-    execute: async (str) => {
-        pool.connect()
-        await pool.execute(str);
+        return await pool.query(str, optArray);
     }
 };
 
